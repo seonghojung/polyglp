e8b2ccaaf9f3dfcc28c29aedb241a69718d46ce9
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-03-18 21:30:17 +0900

    글로벌스타일 추가

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { router } from "./Router";
 import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
+import { GlobalStyle } from "./globalStyle";
 
 function App() {
   return (
     <RecoilRoot>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </RecoilRoot>
   );
