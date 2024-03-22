dfa7744f18087cc0235c03aca3839e8e03e92dd8
CODESPACE BISHOP <74643175+Yoonyonggeun@users.noreply.github.com>
2024-03-22 10:45:55 +0900

    사이드바 cs 알림, 고객문의 알림 처리 완료

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,15 +1,48 @@
 import routes from "../routes";
+import pool from "../db";
 
 // 홈 Home
 export const postCheckAlarmCount = async (req, res) => {
   try {
-    // console.log(body);
+    let evilReportAlarmCount = 0;
+    let qnaAlarmCount = 0;
 
-    // TODO: json 반환 처리에대한 코드 통일
-    res.status(200).json({ msg: "" });
-    // res.json({ msg: "success" });
+    const evilReportsQuery = "SELECT * FROM evil_reports";
+    const qnaQuery = "SELECT * FROM qna";
+
+    const poolQueryFunc = (query, type) => {
+      pool.query(query, (err, result) => {
+        if (err) {
+          console.error("Error executing query", err);
+          res.status(500).send("Error executing query");
+          return;
+        }
+
+        if (result.rows.length !== 0) {
+          result.rows.forEach((x) => {
+            if (type === "evilReports") {
+              if (x.process === 0) {
+                evilReportAlarmCount += 1;
+              }
+            } else if (type === "qna") {
+              if (x.status === "접수(대기)") {
+                qnaAlarmCount += 1;
+              }
+            }
+          });
+        }
+      });
+    };
+
+    poolQueryFunc(evilReportsQuery, "evilReports");
+    poolQueryFunc(qnaQuery, "qna");
+
+    res.status(200).json({ msg: "success", evilReportAlarmCount, qnaAlarmCount });
   } catch (err) {
     console.log(err);
     res.status(500).json({ msg: `server error: ${err}` });
   }
 };
+
+// TODO: 빨간줄 제거용 추후 삭제 필수
+export const any = () => {};
