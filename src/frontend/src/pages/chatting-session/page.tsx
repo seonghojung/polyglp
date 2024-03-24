6697233eafe150fa5e14413a9d39724267a96d7b
codespace-jake <jake@codespace.im>
2024-03-24 12:48:25 +0900

    templete -> template 수정

--- src/frontend/src/pages/chatting-session/page.tsx
+++ src/frontend/src/pages/chatting-session/page.tsx
@@ -1,6 +1,12 @@
+import Template from "../components/Template";
+
 export default function ChattingSessionPage() {
   // 아트보드 : 6. 채팅방 세션
   // WBS : 채팅방 세션
 
-  return <div>채팅방 세션</div>;
+  return (
+    <Template title="채팅방 세션">
+      <div>채팅방세션</div>
+    </Template>
+  );
 }
--- src/frontend/src/pages/payment/page.tsx
+++ src/frontend/src/pages/payment/page.tsx
@@ -1,6 +1,75 @@
+import styled from "styled-components";
+import Template from "../components/Template";
+import Divider from "@mui/material/Divider";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { useState } from "react";
+import TableHeader from "../cs/components/TableHeader";
+
+const rows: GridRowsProp = [
+  { id: 1, col1: "Hello", col2: "World", col3: "asdasd@naver.com" },
+  { id: 2, col1: "DataGridPro", col2: "is Awesome", col3: "asdasd@naver.com" },
+  { id: 3, col1: "MUI", col2: "is Amazing", col3: "asdasd@naver.com" },
+];
+
+const columns: GridColDef[] = [
+  { field: "col1", headerName: "Column 1", width: 150 },
+  { field: "col2", headerName: "Column 2", width: 150 },
+  { field: "col3", headerName: "Column 3", width: 150 },
+];
+
+const TopContent = styled.div`
+  /* height: 232px; */
+`;
+
+const BodyContent = styled.div`
+  /* width: 93.508287%; */
+`;
+
 export default function PaymentsPage() {
   // 아트보드 : 4. 결제 통계
   // WBS : 결제 통계
-
-  return <div>결제 통계</div>;
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 });
+  return (
+    <Template title="결제 통계">
+      <>
+        <TopContent></TopContent>
+        <BodyContent>
+          <div style={{ height: 300, width: "100%" }}>
+            <DataGrid
+              sx={{
+                "& .MuiDataGrid-cell:focus-within, &.MuiDataGrid-cell:focus": {
+                  outline: "none",
+                },
+                "& .MuiDataGrid-columnHeader:focus-within, &.MuiDataGrid-columnHeader:focus": {
+                  outline: "none",
+                },
+                "&.MuiDataGrid-root": {
+                  border: "none",
+                },
+                "& .MuiDataGrid-sortIcon": {
+                  display: "none",
+                },
+                "& .MuiDataGrid-menuIconButton": {
+                  display: "none",
+                },
+                "& .MuiDataGrid-row.Mui-hovered": {
+                  backgroundColor: "transparent",
+                },
+                "& .MuiDataGrid-row:hover": {
+                  backgroundColor: "transparent",
+                },
+              }}
+              autoHeight
+              rows={rows}
+              columns={columns}
+              disableRowSelectionOnClick
+              pageSizeOptions={[10, 25, 50]}
+              paginationModel={paginationModel}
+              onPaginationModelChange={setPaginationModel}
+            />
+          </div>
+        </BodyContent>
+      </>
+    </Template>
+  );
 }
