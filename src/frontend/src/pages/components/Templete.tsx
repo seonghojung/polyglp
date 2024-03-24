36bf1ea30459e2a7c40d477c1631f09cb3caff8e
codespace-jake <jake@codespace.im>
2024-03-24 11:58:35 +0900

    템플릿 수정

--- src/frontend/src/pages/components/Templete.tsx
+++ src/frontend/src/pages/components/Templete.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
   padding: 54px 59px 59px 60px;
@@ -6,8 +6,6 @@ const Container = styled.div`
   margin: 0 auto;
   /* margin: 54px auto 59px; */
 `;
-
-const Wrap = styled.div``;
 
 const Header = styled.div`
   padding-bottom: 15px;
@@ -20,13 +18,27 @@ const Title = styled.span`
   border-bottom: 4px solid #280559;
 `;
 
+const Wrap = styled.div`
+  /* width: 89.108%; */
+
+  margin-top: 102px;
+  /* height: calc(100vh - 273px); */
+  background-color: #fff;
+`;
+
+const Inner = styled.div`
+  padding: 0 47px;
+`;
+
 const Templete = ({ children, title }: { children: React.ReactElement; title: string }) => {
   return (
     <Container>
       <Header>
         <Title>{title}</Title>
       </Header>
-      {children}
+      <Wrap>
+        <Inner>{children}</Inner>
+      </Wrap>
     </Container>
   );
 };
