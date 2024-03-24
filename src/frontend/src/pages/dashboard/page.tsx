e9e560f6e9feb751cd12910183b9cacc13c0c8a0
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-03-24 15:50:16 +0900

    대시보드 그래프

--- src/frontend/src/pages/dashboard/page.tsx
+++ src/frontend/src/pages/dashboard/page.tsx
@@ -1,6 +1,167 @@
+import * as echarts from "echarts/core";
+import { GridComponentOption, LegendComponentOption, TitleComponentOption, TooltipComponentOption } from "echarts/components";
+import { LineSeriesOption, PieSeriesOption } from "echarts/charts";
+import EChartsReact from "echarts-for-react";
+import { addComma } from "../../utils";
+
+export type EChartsOption = echarts.ComposeOption<
+  GridComponentOption | LineSeriesOption | TitleComponentOption | TooltipComponentOption | LegendComponentOption | PieSeriesOption
+>;
 export default function DashBoardPage() {
   // 아트보드 : 0. 대시보드
   // WBS : 대시보드
+  const lineOptions: EChartsOption = {
+    xAxis: {
+      type: "category",
+      data: ["1월", "2월", "3월", "4월", "5월", "6월", "7월"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [3200, 1230, 3224, 2560, 3480, 1800, 3260],
+        type: "line",
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        animation: false,
+      },
+      formatter: (params: any) => {
+        const dataPoint = params[0];
+        return `${dataPoint.axisValueLabel} : ${addComma(dataPoint.data)}명`;
+      },
+    },
+  };
+  const languageOption: EChartsOption = {
+    title: {
+      text: "사용자 별 언어 분포도",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c}명 ({d}%)",
+    },
+    legend: {
+      orient: "vertical",
+      right: 10,
+      top: "center",
+    },
+    series: [
+      {
+        name: "사용 언어",
+        type: "pie",
+        radius: "50%",
+        data: [
+          { value: 1048, name: "한국어" },
+          { value: 735, name: "영어" },
+          { value: 580, name: "중국어(번체)" },
+          { value: 484, name: "말레이 타갈로그어" },
+          { value: 300, name: "히브리어" },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
+  const BusinessOption: EChartsOption = {
+    title: {
+      text: "비즈니스 채팅",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c}명 ({d}%)",
+    },
+    legend: {
+      orient: "vertical",
+      right: 10,
+      top: "center",
+    },
+    series: [
+      {
+        name: "사용 언어",
+        type: "pie",
+        radius: "50%",
+        data: [
+          { value: 1048, name: "한국어 x 영어" },
+          { value: 735, name: "영어 x 중국어(번체)" },
+          { value: 580, name: "중국어(번체) x 일본어" },
+          { value: 484, name: "말레이 타갈로그어 x 스와힐리어" },
+          { value: 300, name: "히브리어 x 고대 이집트어" },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
+  const RandomOption: EChartsOption = {
+    title: {
+      text: "랜덤 채팅",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c}명 ({d}%)",
+    },
+    legend: {
+      orient: "vertical",
+      right: 10,
+      top: "center",
+    },
+    series: [
+      {
+        name: "사용 언어",
+        type: "pie",
+        radius: "50%",
+        data: [
+          { value: 1048, name: "한국어 x 영어" },
+          { value: 236, name: "영어 x 중국어(번체)" },
+          { value: 343, name: "중국어(번체) x 일본어" },
+          { value: 1525, name: "말레이 타갈로그어 x 스와힐리어" },
+          { value: 347, name: "히브리어 x 고대 이집트어" },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
 
-  return <div>대시보드</div>;
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <EChartsReact option={lineOptions} opts={{ renderer: "canvas", width: "auto", height: "auto" }} />
+        </div>
+        <div style={{ flex: 1 }}>
+          <EChartsReact option={languageOption} opts={{ renderer: "canvas", width: "auto", height: "auto" }} />
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <EChartsReact option={BusinessOption} opts={{ renderer: "canvas", width: "auto", height: "auto" }} />
+        </div>
+        <div style={{ flex: 1 }}>
+          <EChartsReact option={RandomOption} opts={{ renderer: "canvas", width: "auto", height: "auto" }} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/routers/dashboardRouter.js
+++ src/routers/dashboardRouter.js
@@ -1,22 +1,55 @@
 import express from "express";
 import client from "../db";
-import { BASE_URL } from "../util";
 
 const dashboardRouter = express.Router();
 
-dashboardRouter.get("/dashboard", async (req, res) => {
-  // 1. 총 영상통화시간 --- @TODO: 계산 기준 문의필요
-  // 2. 비즈니스 영상 통화 시간 --- @TODO: 계산 기준 문의필요
-  // 3. 랜덤 영상 통화 시간 --- @TODO: 계산 기준 문의필요
-  // 4. 총 회원 수
-  const totalUsers = await client.query("SELECT count(*) FROM users");
-  // 5. 무료 회원 수
-  const subscribedUsers = await client.query("SELECT count(*) FROM users where issubscription=true");
-  // 6. 구독 회원 수
-  const normalUsers = await client.query("SELECT count(*) FROM users where issubscription=false");
+// 대시보드 - 통합 지표 표기
+dashboardRouter.get("/integratedIndex", async (req, res) => {
+  try {
+    // 1. 총 영상통화시간 --- @TODO: 계산 기준 문의필요
+    const {
+      rows: [{ count: totalVideoCallTime }],
+    } = { rows: [{ count: 0 }] };
 
-  res.json({ totalUsers: totalUsers.rows[0].count, subscribedUsers: subscribedUsers.rows[0].count, normalUsers: normalUsers.rows[0].count });
+    // 2. 비즈니스 영상 통화 시간 --- @TODO: 계산 기준 문의필요
+    const {
+      rows: [{ count: businessVideoCallTime }],
+    } = { rows: [{ count: 0 }] };
+
+    // 3. 랜덤 영상 통화 시간 --- @TODO: 계산 기준 문의필요
+    const {
+      rows: [{ count: randomVideoCallTime }],
+    } = { rows: [{ count: 0 }] };
+
+    // 4. 총 회원 수
+    const {
+      rows: [{ count: totalUsers }],
+    } = await client.query("SELECT count(*) FROM users");
+
+    // 5. 무료 회원 수
+    const {
+      rows: [{ count: subscribedUsers }],
+    } = await client.query("SELECT count(*) FROM users where issubscription=true");
+
+    // 6. 구독 회원 수
+    const {
+      rows: [{ count: normalUsers }],
+    } = await client.query("SELECT count(*) FROM users where issubscription=false");
+
+    return res.status(200).json({ totalVideoCallTime, businessVideoCallTime, randomVideoCallTime, totalUsers, subscribedUsers, normalUsers });
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 });
+
+// 대시보드 - 트렌드 지표
+dashboardRouter.get("/trendIndex", async (req, res) => {});
+
+// 대시보드 - 언어 별 매칭 통계
+dashboardRouter.get("/languageMatchingIndex", async (req, res) => {});
+
+// 대시보드 - APP 통계
+dashboardRouter.get("/appIndex", async (req, res) => {});
 
 dashboardRouter.use((_, res) => {
   res.status(405).send("[/dashboard] - Method Not Allowed");
